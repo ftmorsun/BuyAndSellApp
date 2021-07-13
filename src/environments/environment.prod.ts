90213f13f3bec780c1ea11b310464d7651e5690d
Fatma Orsun <fatmaorsun2@gmail.com>
2021-07-12 22:03:13 -0400

    removed production details

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,4 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: "AIzaSyDUy873f60DIzCZ3kjyLjsvAlLc-IGVd4c",
-    authDomain: "buysell-51f2a.firebaseapp.com",
-    projectId: "buysell-51f2a",
-    storageBucket: "buysell-51f2a.appspot.com",
-    messagingSenderId: "931232699520",
-    appId: "1:931232699520:web:5118f6be27ff4bd0b4c265"
 
-  }
 };
